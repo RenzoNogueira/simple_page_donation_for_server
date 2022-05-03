ce122606cc63acda95dac9aced3696487c42ee36
RenzoNogueira <mainrenzossn@gmail.com>
2022-05-03 00:03:47 -0300

    Arquivo para buscar doações para a contagem criado; Fonte do modal alterada; id do modal alterado.

--- php/getTargetValue.php
+++ php/getTargetValue.php
@@ -1,9 +1,5 @@
 <?php
 require '../php/host/host.php';
-
-// tabela savings_goal
 $sql = "SELECT * FROM savings_goal";
-
 $result = mysqli_query($conexao, $sql);
-
 echo json_encode(mysqli_fetch_all($result, MYSQLI_ASSOC));
